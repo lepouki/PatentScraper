68d812d5bb7bf353a36c2009955dc91490f48627
wakfulily <wakfulily@gmail.com>
2020-04-14 17:25:44 +0200

    Fixed claim count processor

--- src/scraper/core/processors/ClaimCountProcessor.java
+++ src/scraper/core/processors/ClaimCountProcessor.java
@@ -1,5 +1,7 @@
 package scraper.core.processors;
 
+import org.jsoup.nodes.Element;
+
 public class ClaimCountProcessor extends IntegerPagePropertyProcessor {
 
 	private static final String PROPERTY_NAME = "Claim count";
@@ -10,7 +12,24 @@ public class ClaimCountProcessor extends IntegerPagePropertyProcessor {
 
 	@Override
 	protected int getValue() throws NoSuchPropertyException {
-		return select("li[class=claim]").size();
+		String selector = HasClaimsProcessor.getClaimsSectionSelector();
+		Element claimsSection = selectFirst(selector);
+		return getClaimCountInClaimsSection(claimsSection);
+	}
+
+	private int getClaimCountInClaimsSection(Element claimsSection) {
+		Element claimCountElement = claimsSection.selectFirst("span[itemprop=count]");
+
+		if (claimCountElement == null) {
+			return 0;
+		}
+
+		return parseClaimCountElement(claimCountElement);
+	}
+
+	private int parseClaimCountElement(Element claimCountElement) {
+		String claimCountText = claimCountElement.ownText();
+		return Integer.parseInt(claimCountText);
 	}
 
 }
