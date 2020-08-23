7a69d633178673e4c1da5efe6718c4d78a29f61a
wakfulily <wakfulily@gmail.com>
2020-08-23 14:22:34 +0200

    Changed options per row back to 4

--- src/scraper/application/LayoutConfiguration.java
+++ src/scraper/application/LayoutConfiguration.java
@@ -3,6 +3,6 @@ package scraper.application;
 public class LayoutConfiguration {
 
 	public static final int PADDING = 10;
-	public static final int OPTIONS_PER_ROW = 3;
+	public static final int OPTIONS_PER_ROW = 4;
 
 }
